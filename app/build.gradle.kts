be343755ab2ccea5d7299dbc818c28692e2d6cc7
konye <konye@youverify.co>
2021-06-10 00:58:44 +0100

    update project level build.gradle.kts and app level build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id(Plugins.androidApplication)
+    kotlin(Plugins.kotlinAndroid)
+    kotlin(Plugins.kotlinAndroidExtensions)
+    kotlin(Plugins.kotlinKapt)
+}
+
+android {
+    compileSdkVersion(Configs.Versions.compileSdk)
+
+    defaultConfig {
+        applicationId = Configs.applicationId
+        minSdkVersion(Configs.Versions.minSdk)
+        targetSdkVersion(Configs.Versions.targetSdk)
+        versionCode = Configs.versionCode
+        versionName = Configs.versionName
+
+        testInstrumentationRunner = Configs.testInstrumentationRunner
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("debug") {
+            versionNameSuffix = "-dev"
+            applicationIdSuffix = ".debug"
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(project(":core"))
+    implementation(project(":lib:commons"))
+    implementation(project(":lib:navigation"))
+    implementation(project(":lib:resources"))
+    implementation(project(":feature:cars"))
+    implementation(project(":feature:cardetail"))
+
+    implementation(Dependencies.Kotlin.stdlib)
+
+    // AndroidX
+    implementation(Dependencies.AndroidX.coreKtx)
+    implementation(Dependencies.AndroidX.constraintlayout)
+    implementation(Dependencies.AndroidX.Navigation.fragment)
+    implementation(Dependencies.AndroidX.Navigation.ui)
+
+    // Dagger
+    implementation(Dependencies.Dagger.dagger)
+    kapt(Dependencies.Dagger.compiler)
+
+    implementation(Dependencies.Google.material)
+
+    testImplementation(project(":lib:testcommons"))
+}
---
+++ build.gradle.kts
@@ -0,0 +1,109 @@
+
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryPlugin
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+plugins {
+    id(Plugins.spotless) version Versions.spotless
+}
+
+buildscript {
+    val kotlin_version by extra("1.4.31")
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath(Classpaths.gradle)
+        classpath(Classpaths.kotlinGradle)
+        classpath(Classpaths.safeArgs)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+subprojects {
+
+    project.pluginManager.apply(Plugins.spotless)
+
+    spotless {
+        java {
+            // This is required otherwise the code in android modules isn"t picked up by spotless.
+            target("**/*.java")
+            trimTrailingWhitespace()
+            removeUnusedImports()
+            googleJavaFormat()
+        }
+
+        kotlin {
+            target("**/*.kt")
+            ktlint("0.29.0").userData(hashMapOf("indent_size" to "4", "android" to "true", "max_line_length" to "200"))
+        }
+
+        kotlinGradle {
+            target("**/*.build.gradle.kts.kts.kts.kts.kts.kts.kts.kts.kts.kts.kts.kts.kts.kts.kts")
+            ktlint("0.29.0").userData(hashMapOf("indent_size" to "4", "android" to "true", "max_line_length" to "200"))
+        }
+
+        format("misc") {
+            target("**/.gitignore", "**/*.gradle", "**/*.md", "**/*.sh", "**/*.yml")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+    }
+
+    project.tasks.withType(KotlinCompile::class.java) {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict", "-progressive")
+            jvmTarget = "1.8"
+        }
+    }
+
+    project.plugins.whenPluginAdded {
+        when (this) {
+            is AppPlugin, is LibraryPlugin -> {
+                the<BaseExtension>().apply {
+                    compileSdkVersion(Configs.Versions.compileSdk)
+
+                    defaultConfig {
+                        minSdkVersion(Configs.Versions.minSdk)
+                        targetSdkVersion(Configs.Versions.targetSdk)
+                    }
+
+                    compileOptions {
+                        targetCompatibility = JavaVersion.VERSION_1_8
+                        sourceCompatibility = JavaVersion.VERSION_1_8
+                    }
+
+                    sourceSets{
+                        getByName("main") {
+                            java.srcDir("src/main/kotlin")
+                        }
+                        getByName("test") {
+                            java.srcDir("src/test/kotlin")
+                        }
+                        getByName("androidTest") {
+                            java.srcDir("src/androidTest/kotlin")
+                        }
+                    }
+                }
+            }
+
+            is JavaPlugin -> {
+                the<JavaPluginConvention>().apply {
+                    sourceCompatibility = JavaVersion.VERSION_1_8
+                    targetCompatibility = JavaVersion.VERSION_1_8
+                }
+            }
+        }
+    }
+}
